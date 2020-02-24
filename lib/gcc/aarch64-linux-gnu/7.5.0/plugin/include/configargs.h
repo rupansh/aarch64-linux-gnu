c367af208fa23e28f5c1f5d37ee023750149dfbb
Rupansh Sekar <rupanshsekar@hotmail.com>
2020-02-24 16:16:49 +0530

    aarch64-linux-gnu
    GCC 7.5.0, glibc-2.3.1, binutils 2.31.1, linux headers from v5.6-rc3
    
    Signed-off-by: Rupansh Sekar <rupanshsekar@hotmail.com>

---
+++ lib/gcc/aarch64-linux-gnu/7.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-7.5.0/configure --prefix=/Volumes/android/cross --target=aarch64-linux-gnu --enable-languages=c,c++ --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
